0bed5420fcc15bad2295deca2d5df58b58c0d73c
adriancs <avoomj@yahoo.com>
2022-12-04 21:12:44 +0800

    create nuget package

--- source code/html-pdf-edge/html-pdf-edge-class/pdf_edge.cs
+++ source code/html-pdf-edge/html-pdf-edge-class/pdf_edge.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Web;
 
-namespace html_pdf_edge
+namespace System
 {
     public class pdf_edge
     {
